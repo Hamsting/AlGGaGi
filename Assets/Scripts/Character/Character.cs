b9dfd97d0a1ad77eaf17b47fbb1ba097cedef543
Hamsting <qudgus6214@gmail.com>
2017-05-21 23:25:42 +0900

    여러가지 작업 중

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Shared/Bar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+	private Image img;
+
+
+
+	public void Initialize()
+	{
+		img = this.GetComponent<Image>();
+	}
+
+	public void Tick(float _percent01)
+	{
+		img.fillAmount = _percent01;
+	}
+}
